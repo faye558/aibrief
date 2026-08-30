26f980d3d565569dd32b98d807f672b92756d027
DOSUNGLOK <dosunglok@DOSUNGLOKui-MacBookAir.local>
2026-08-30 19:24:10 +0900

    fix: GA4 gtag 초기화 스크립트 변수 치환 오류 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6383180248696987" crossOrigin="anonymous" strategy="afterInteractive" />
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
-        <Script id="ga4-init" strategy="afterInteractive">{`
+        <Script id="ga4-init" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_ID}');
-        `}</Script>
+        ` }} />
       </head>
       <body>
         <script dangerouslySetInnerHTML={{ __html: `
